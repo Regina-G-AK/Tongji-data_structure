2df4e4387debf064966ff97dd740b814878e98bb
Fangming Guo <guoregina03@gmail.com>
2024-11-27 16:53:08 +0800

    4.2 小世界现象

--- HW4/HW4_2.cpp
+++ HW4/HW4_2.cpp
@@ -2,30 +2,31 @@
 #include <vector>
 #include <queue>
 #include <iomanip>
-
 using namespace std;
 
+vector<vector<int>> adj_list;
+
 // BFS函数，计算从起点start出发六步以内的可达节点数
-int bfs(int start, int n, vector<vector<int>>& adj_list) 
+int bfs(int start, int n, vector<vector<int>>& adj_list)
 {
-    vector<bool> visited(n, false);
-    vector<int> distance(n, -1);
+    vector<bool> visited(n + 1, false);
+    vector<int> distance(n + 1, -1);
     queue<int> q;
     q.push(start);
     visited[start] = true;
     distance[start] = 0;
     int count = 1; // 起点本身
-    while (!q.empty()) 
+    while (!q.empty())
     {
         int u = q.front();
         q.pop();
-        for (int v : adj_list[u]) 
+        for (int v : adj_list[u])
         {
-            if (!visited[v]) 
+            if (!visited[v])
             {
                 visited[v] = true;
                 distance[v] = distance[u] + 1;
-                if (distance[v] <= 6) 
+                if (distance[v] <= 6)
                 {
                     q.push(v);
                     count++;
@@ -36,12 +37,12 @@ int bfs(int start, int n, vector<vector<int>>& adj_list)
     return count;
 }
 
-int main() 
+int main()
 {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> adj_list(n);
-    for (int i = 0; i < m; i++) 
+    adj_list.resize(n + 1);
+    for (int i = 0; i < m; i++)
     {
         int a, b;
         cin >> a >> b;
@@ -50,11 +51,11 @@ int main()
     }
 
     // 对每个节点计算六度空间内的可达节点数占总节点数的百分比
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
-        int reachable = bfs(i, n, adj_list);
+        int reachable = bfs(i + 1, n, adj_list);
         float percent = (float)reachable / n * 100;
-        cout << fixed << setprecision(2) << percent << endl;
+        cout << i + 1 << ": " << fixed << setprecision(2) << percent << "%" << endl;
     }
 
     return 0;
