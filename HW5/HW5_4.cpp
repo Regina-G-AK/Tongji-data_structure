45fcbb938656663e1d4df8112a7ed0012c8834a8
Fangming Guo <guoregina03@gmail.com>
2024-12-15 14:24:05 +0800

    5.2 二叉排序树

---
+++ HW5/HW5_4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int n,p;
+    cin>>n>>p;
+    string s;
+    map<string,int> mp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s;
+        
+    }
+
+
+    return 0;
+}
